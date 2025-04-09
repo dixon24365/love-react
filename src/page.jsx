583a05194d46eac31e21ca5c06804dff4497ec79
dixon24365 <tuedin.james@gmail.com>
2025-04-09 23:33:37 +0300

    added header and footer

--- src/page.jsx
+++ src/page.jsx
@@ -1,12 +1,21 @@
 function Page()  {
     return(
         <>
+        <header>
+            <img src="./react-logo.png" width="100" alt="react-logo"></img>
+        </header>
+        <main>
+        <h1>Why I am excited to learn React</h1>
         <ol>
             <li>React is cool</li>
             <li>I just wanna get better at coding</li>
             <li>Wanna make big moneys some day</li>
             <li>I'm tryna figure out what my purpose on earth is, it could be creating React Apps</li>
         </ol>
+        </main>
+        <footer>
+            &copy; 2025 Dixon. All Rights Reserved.
+        </footer>
         </>
     )
 }
