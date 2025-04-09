4ac2df5591c670ca18065d4b8fb3055de7a853e4
dixon24365 <tuedin.james@gmail.com>
2025-04-09 23:11:05 +0300

    added pages component

---
+++ src/page.jsx
@@ -0,0 +1,13 @@
+function Page()  {
+    return(
+        <>
+        <ol>
+            <li>React is cool</li>
+            <li>I just wanna get better at coding</li>
+            <li>Wanna make big moneys some day</li>
+            <li>I'm tryna figure out what my purpose on earth is, it could be creating React Apps</li>
+        </ol>
+        </>
+    )
+}
+export default Page
